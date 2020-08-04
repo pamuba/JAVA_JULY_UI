7560f9659e917e3f471e4aee974ee165f8d4d7ff
pamuba <pamuba@users.noreply.github.com>
2020-08-04 23:29:10 +0530

    71

--- AngularJS/app8.js
+++ AngularJS/app8.js
@@ -1,1 +1,21 @@
-var app = angular.module('app',[]);
+var app = angular.module('app',['ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider){
+    $routeProvider
+    .when('/first-msg', {
+        // template: '<strong>This is first message</strong>'
+        templateUrl:'msg1.html',
+        controller:'message1'
+    })
+    .when('/second-msg', {
+        template: '<strong>This is second message</strong>'
+    })
+    .when('/', {
+        template: '<strong>Default Route</strong>'
+    })
+}]);
+
+app.controller('message1', ['$scope', function($scope){
+    $scope.a = 10;
+    $scope.b = 20;
+}])
