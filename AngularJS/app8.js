35474256794ad278db9239a5d06d25accc14a680
pamuba <pamuba@users.noreply.github.com>
2020-08-04 23:59:57 +0530

    72

--- AngularJS/app8.js
+++ AngularJS/app8.js
@@ -2,20 +2,58 @@ var app = angular.module('app',['ngRoute']);
 
 app.config(['$routeProvider', function($routeProvider){
     $routeProvider
-    .when('/first-msg', {
+    .when('/sumurl/:a/:b', {
+        templateUrl:'sumurl.html',
+        controller:'sumurl',
         // template: '<strong>This is first message</strong>'
-        templateUrl:'msg1.html',
-        controller:'message1'
+        // templateUrl:'msg1.html',
+        // controller:'message1',
+        // caseInsensitiveMatch:true
     })
-    .when('/second-msg', {
-        template: '<strong>This is second message</strong>'
+    .when('/suminput', {
+        templateUrl:'suminput.html',
+        controller:'suminput',
     })
-    .when('/', {
-        template: '<strong>Default Route</strong>'
+    .otherwise({
+        template: '<strong>Wrong route Choosen</strong>'
     })
+    // .when('/second-msg', {
+    //     // template: '<strong>This is second message</strong>'
+    //     templateUrl:'msg2.html',
+    //     controller:'message2'
+    // })
+    // .when('/', {
+    //     // template: '<strong>Click one of the Links</strong>'
+    //     redirectTo: function(){
+    //         alert("Not implemented yet");
+    //         return '/first-msg'
+    //     }
+    // })
+   
 }]);
 
-app.controller('message1', ['$scope', function($scope){
-    $scope.a = 10;
-    $scope.b = 20;
+// app.controller('message1', ['$scope', function($scope){
+//     $scope.a = 10;
+//     $scope.b = 20;
+// }])
+
+// app.controller('message2', ['$scope', function($scope){
+//     $scope.c = 30;
+//     $scope.d = 40;
+// }])
+
+app.controller('sumurl', ['$scope', '$routeParams', function($scope, $routeParams){
+    $scope.a = $routeParams.a;
+    $scope.b = $routeParams.b;
+}])
+
+app.controller('suminput', ['$scope', '$location', '$interpolate', function($scope, $location, $interpolate){
+    $scope.a = 0;
+    $scope.b = 0;
+
+    $scope.doSum = function(){
+        var url = $interpolate('/sumurl/{{a}}/{{b}}')($scope)
+        // console.log(url)
+        $location.path(url);
+    }
 }])
