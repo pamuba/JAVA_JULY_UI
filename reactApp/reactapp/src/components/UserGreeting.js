cb8cc486bc0682d24c2c9f0cec35065aa7e4d05f
pamuba <pamuba@users.noreply.github.com>
2020-08-15 23:27:24 +0530

    1

--- reactApp/reactapp/src/components/UserGreeting.js
+++ reactApp/reactapp/src/components/UserGreeting.js
@@ -11,15 +11,29 @@ class UserGreeting extends Component {
     
     render() {
 
-        let message
-        if(this.state.isLoggedIn){
-            message = <div>Welcome User</div>
-        }
-        else{
-            message = <div>Welcome Guest</div>
-        }
+        return this.state.isLoggedIn && <div>Welcome User</div>
 
-        return <div>{message}</div>
+        // return(
+        //     this.state.isLoggedIn ? 
+        //     <div>Welcome User</div> :
+        //     <div>Welcome Guest</div>
+        // )
+
+
+        // let message
+        // if(this.state.isLoggedIn){
+        //     message = <div>Welcome User</div>
+        // }
+        // else{
+        //     message = <div>Welcome Guest</div>
+        // }
+
+        // return <div>{message}</div>
+
+
+
+
+
         // if(this.state.isLoggedIn){
         //     return(
         //         <div>Welcome User</div>
@@ -27,4 +41,10 @@ class UserGreeting extends Component {
         // }
         // else{
         //     return (
-    
+        //         <div>Welcome Guest</div>
+        //     )
+        // }
+    }
+}
+
+export default UserGreeting
